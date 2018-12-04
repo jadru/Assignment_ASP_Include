3452741f056970d0250c152706e7bfae9763c67c
jadru <ygpark0922@gmail.com>
2018-12-05 02:25:01 +0900

    마스터 변경

--- _include_/_include_/Master.master.cs
+++ _include_/_include_/Master.master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 public partial class Master : System.Web.UI.MasterPage
 {
@@ -11,6 +12,31 @@ public partial class Master : System.Web.UI.MasterPage
     {
         if (Application["islogin"].ToString() == "true")
         {
+            string connectionString = @"server=(local)\SQLExpress;Integrated Security=true;database=db_user";
+            SqlConnection Con = new SqlConnection(connectionString);
+
+            // SQL COMMAND OBJECT를 만들고  SQL COMMAND 넣기
+            SqlCommand Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+            Cmd.CommandText = "SELECT * FROM db_user WHERE id = '" + Application["id"].ToString() + "'";
+            try
+            {
+                Con.Open();
+                // ExecuteReader()는 SELECT 문을 사용하여 RECORD를 가져올때 사용
+                SqlDataReader reader = Cmd.ExecuteReader();
+
+                // SqlDataReader의 기본 위치는 첫 번째 레코드 앞, Read( )를 호출하여 하나의 레코드 읽기 
+                // read.Read( )의 반환 값 형식: Boolean, 행이 더 있으면 true이고, 그렇지 않으면 false입니다. 
+                while (reader.Read())
+                {
+                    Application["name"] = reader["name"];
+                    Application["id"] = reader["id"];
+                    Application["email"] = reader["email"].ToString().Trim();
+                }
+                reader.Close();
+                Con.Close();
+            }
+            catch { }
             LinkButton1.Text = Application["name"].ToString() + "님 정보";
             LinkButton2.Text = "로그아웃";
         }
